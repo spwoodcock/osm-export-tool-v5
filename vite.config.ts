232b2f54d0f0099d3df043b26dc4b85d803f19c4
spwoodcock <sam.woodcock@protonmail.com>
2025-10-04 19:21:12 +0100

    build: use vite config instead of svelte for base

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import UnoCSS from 'unocss/vite';
 
-// https://vite.dev/config/
+const repoName =
+  process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
+
 export default defineConfig({
+  // Append suffix for github pages deploy if CI
+  base: repoName ? `/${repoName}/` : '/',
   plugins: [svelte(), UnoCSS()],
   server: {
     host: '0.0.0.0',
     port: 3000
-  },
+  }
 })
