ccf9492aebde38ca4c0deba50e5a043caf00d574
spwoodcock <sam.woodcock@protonmail.com>
2025-10-04 19:11:31 +0100

    fix: allow hosting via github pages subpath

--- svelte.config.js
+++ svelte.config.js
@@ -5,4 +5,7 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  base: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/',
 }
